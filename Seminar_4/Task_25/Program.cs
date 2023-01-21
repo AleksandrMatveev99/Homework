cbff5daf6d585a8d2f545bdea7d793eb0de63716
Aleksandr Matveev <aleksandrmatveev99@yandex.ru>
2023-01-21 12:53:34 +0300

    Add Task_25

---
+++ Seminar_4/Task_25/Program.cs
@@ -0,0 +1,24 @@
+﻿// Напишите цикл, который принимает на вход два числа (A и B) и возводит число A в натуральную степень B.
+
+int Exponentiation(int A, int B)
+{
+    int result = 1;
+    for (int i = 0; i < B; i++)
+    {
+        result*= A;
+    }
+    return result;
+}
+Console.Write("Введите число A: ");
+int number = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите число B: ");
+int degree = Convert.ToInt32(Console.ReadLine());
+if (degree < 1)
+{
+    Console.WriteLine($"Степень {degree} не является натуральной");
+    return;
+}
+int answer = Exponentiation(number, degree);
+Console.WriteLine(answer);
+
+// Использовал метод для практики
