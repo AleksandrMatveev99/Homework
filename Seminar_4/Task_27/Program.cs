48ce10fdf570902d1238a01c459be93e026b32d6
Aleksandr Matveev <aleksandrmatveev99@yandex.ru>
2023-01-21 13:28:44 +0300

    Add Task_27

---
+++ Seminar_4/Task_27/Program.cs
@@ -0,0 +1,23 @@
+﻿// Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.
+
+int ReadNumber(string message)
+{
+    Console.Write(message);
+    return Convert.ToInt32(Console.ReadLine());
+}
+
+int SumDigit(int remainingNumber)
+{
+    int sum = 0;
+    while (remainingNumber != 0)
+    {
+        sum = sum + remainingNumber % 10;
+        remainingNumber = remainingNumber / 10;
+    }
+    return sum;
+}
+
+int number = ReadNumber("Введите число: ");
+if (number < 0) number *= -1;
+int summa = SumDigit(number);
+Console.WriteLine(summa);
