b2b99fb3e86bd7962eb7f2a95c0d6612cd964ab2
Aleksandr Matveev <aleksandrmatveev99@yandex.ru>
2023-01-21 13:57:16 +0300

    Add Task_29

---
+++ Seminar_4/Task_29/Program.cs
@@ -0,0 +1,15 @@
+﻿// Напишите программу, которая задаёт массив из 8 элементов и выводит их на экран. Данные вводятся с консоли пользователем.
+
+void FillArray(int[] array)
+{
+    int length = array.Length;
+    for (int i = 0; i < length; i++)
+    {
+        Console.Write($"A({i}): ");
+        array[i] = Convert.ToInt32(Console.ReadLine());
+    }
+}
+
+int[] arr = new int[8];
+FillArray(arr);
+Console.WriteLine(string.Join(" ", arr));
