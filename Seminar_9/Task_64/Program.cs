505462edd8ea34f977b8275d99d2053a9ac66ea9
Aleksandr Matveev <aleksandrmatveev99@yandex.ru>
2023-02-20 19:49:03 +0300

    Add Task_64

---
+++ Seminar_9/Task_64/Program.cs
@@ -0,0 +1,19 @@
+﻿// Задайте значение N. Напишите программу, которая выведет все натуральные числа в промежутке от N до 1.
+// N = 5 -> "5, 4, 3, 2, 1"
+// N = 8 -> "8, 7, 6, 5, 4, 3, 2, 1"
+
+string NumberRecursion(int number)
+{
+    if (number == 1) return $"{number}";
+    return $"{number}, " + NumberRecursion(number - 1);
+}
+
+Console.Write("Введите натуральное число N: ");
+int N = Convert.ToInt32(Console.ReadLine());
+if (N <= 1)
+{
+    Console.Write("Число должо быть больше 1");
+    return;
+}
+
+Console.Write(NumberRecursion(N));
